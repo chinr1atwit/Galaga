5c0e88432ba7ad2dbf70f8c30d7e5b04253fbdc2
chinr1atwit <55757904+chinr1atwit@users.noreply.github.com>
2019-12-03 17:18:02 -0500

    Update GameScene.java

--- Game/GameScene.java
+++ Game/GameScene.java
@@ -1,6 +1,8 @@
 public class GameScene
 {
-    public static void main(String[])
+    public static void main(String[] args)
     {
-    }
+        private SpaceShip ship = new Spaceship();
+        public ArrayList<Laser> lasers = new ArrayList<>();
+    } 
 }
