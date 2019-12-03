7dce17662c82fcf42a625b36fe34662f798f593b
chinr1atwit <55757904+chinr1atwit@users.noreply.github.com>
2019-12-03 17:22:23 -0500

    Update Laser.java

--- Game/Laser.java
+++ Game/Laser.java
@@ -1,3 +1,23 @@
 public class Laser
 {
+    private int xcoord, ycoord;
+    
+    public Laser()
+    {
+        xcoord = 0;
+        ycoord = 0;
+    }
+    public Laser(int x, int y)
+    {
+        xcoord = x;
+        ycoord = y;
+    }
+    private void move()
+    {
+        ycoord -= 5;
+        if(this.ycoord < 0)
+            lasers.remove(this);
+        else
+            move();
+    }
 }
